8fe5c44bfde34224bc3343c96146039ff324c0fe
Samay raj <samayraj099@gmail.com>
2026-06-14 03:04:28 +0530

    Use u2netp model

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 ﻿from flask import Flask, request, send_file
 from flask_cors import CORS
-from rembg import remove
-from PIL import Image
+from rembg import remove, new_session
 from dotenv import load_dotenv
 import io
 import os
@@ -9,19 +8,42 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+
+CORS(
+    app,
+    origins=["https://bg-remover-nine-delta.vercel.app"]
+)
 
 PORT = int(os.getenv("PORT", 5000))
 
+session = new_session("u2netp")
+
 @app.route('/remove-bg', methods=['POST'])
 def remove_bg():
-    file = request.files['image']
-    input_data = file.read()
-    output_data = remove(input_data)
-    return send_file(
-        io.BytesIO(output_data),
-        mimetype='image/png'
-    )
+    try:
+        file = request.files['image']
+
+        input_data = file.read()
+
+        output_data = remove(
+            input_data,
+            session=session
+        )
+
+        return send_file(
+            io.BytesIO(output_data),
+            mimetype='image/png'
+        )
+
+    except Exception as e:
+        print("ERROR:", e)
+        return {"error": str(e)}, 500
+
+
+@app.route("/")
+def home():
+    return "Background Remover API Running"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
